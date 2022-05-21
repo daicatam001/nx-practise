aab7f57c9de30db44ca7d1a2ba53c3f1852466f9
tampt <tampt95@gmail.com>
2022-05-21 19:37:54 +0700

    add front project

---
+++ apps/front/src/app/app.js
@@ -0,0 +1,10 @@
+import { Route, Routes, Link } from 'react-router-dom';
+
+export function App() {
+  return (
+    <> 
+    <div className='text-2xl'>dsasd</div>
+     </>
+  );
+}
+export default App;
---
+++ apps/front/tailwind.config.js
@@ -0,0 +1,12 @@
+const { createGlobPatternsForDependencies } = require('@nrwl/react/tailwind');
+const { join } = require('path');
+module.exports = {
+  content: [
+    join(__dirname, 'src/**/*!(*.stories|*.spec).{js,ts,tsx,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
